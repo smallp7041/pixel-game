56a0ea4a762bacad3f26de4afcc9c18cf36986ee
smallp7041 <smallp7041@gmail.com>
2026-01-15 16:15:03 +0800

    Fix vite config import error

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,6 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 // https://vite.dev/config/
 export default defineConfig({
   base: './', // 確保在 GitHub Pages 子路徑也能正常存取
